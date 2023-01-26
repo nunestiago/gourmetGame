b27c3b577c393972eae9d7971339e416f5618dd6
Tiago Nunes <ntiagon@gmail.com>
2023-01-25 21:55:13 -0300

    start implement play again

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
+import java.sql.Array;
 
 public class Main {
     public static void main(String[] args) {
+        String[] foodGenre = new String[]{"massa", "oriental"};
 //        JOptionPane.showMessageDialog(null, "nada", "title", JOptionPane.INFORMATION_MESSAGE);
 //        JOptionPane.showMessageDialog(null, "nada", "title", JOptionPane.ERROR_MESSAGE);
 //
@@ -16,14 +18,23 @@ public class Main {
                     " Vamos adivinhar do que você gosta", gameTitle, JOptionPane.PLAIN_MESSAGE);
 
             // Food Genre Panels
-            int test = JOptionPane.showConfirmDialog(null, "Você gosta de massa?", gameTitle, JOptionPane.YES_NO_OPTION);
+            selectFoodGenre(foodGenre, gameTitle);
+            int plãyAgain = JOptionPane.showConfirmDialog(null, "Não descobrimos do que você gosta. \n" +
+                    "Quer jogar de novo?", gameTitle, JOptionPane.YES_NO_OPTION);
 
-            if (test == JOptionPane.YES_OPTION) {
-                // TODO go to foodDish option
-            }
-            System.out.println(test);
+            if (plãyAgain == JOptionPane.YES_OPTION) selectFoodGenre(foodGenre, gameTitle);
             runProgram = false;
         }
     }
 
+    private static void selectFoodGenre(String[] foodGenre, String gameTitle) {
+        int selectedFoodGenre = 1; // 1 is the NO on showConfirmDialog
+        for (int i = 0; i < foodGenre.length; i++) {
+            selectedFoodGenre = JOptionPane.showConfirmDialog(null, String.format("Você gosta de %s ?", foodGenre[i]), gameTitle, JOptionPane.YES_NO_OPTION);
+            if (selectedFoodGenre == JOptionPane.YES_OPTION) {
+                // TODO go to foodDish option
+                break;
+            }
+        }
+    }
 }

606935ab3f94cc22e1a2ab980d992fa27094851c
Tiago Nunes <ntiagon@gmail.com>
2023-01-25 21:36:35 -0300

    git init

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        JOptionPane.showMessageDialog(null, "nada", "title", JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "nada", "title", JOptionPane.ERROR_MESSAGE);
+//
+//        JOptionPane.showConfirmDialog(null, "question", "title", JOptionPane.YES_NO_OPTION);
+//
+//        JOptionPane.showInputDialog("question");
+        boolean runProgram = true;
+        String gameTitle = "Food Game";
+        while (runProgram) {
+
+            runProgram = false;
+        }
+    }
+
+}
